53cb93f70e4e67f380fbb858650bb2293d6916dc
Folafolu <ofolafolu@gmail.com>
2023-11-21 16:06:00 +0100

    fix

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response, send_file
+from flask import Flask, render_template, request, send_file
 from datetime import datetime
 import pdfkit
 import io
@@ -6,26 +6,24 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('invoiceform.html')
-
-@app.route('/home')
+@app.route('/', methods=['GET', 'POST'])
 def invoice():
+    posted_data = request.get_json() or {}
     today = datetime.today().strftime("%B %d, %Y")
-    details = {
+    default_data = {
+        'details' : {
         'date': today,
         'invoice_number' : 123,
-        'due_date': today
-    }
-    recipient = {
-    'name': 'Acme Corp',
-    'email': 'john@example.com',
-    'addr_1': '12345 Sunny Road',
-    'addr_2' : 'Sunnyville, CA 12345'
-    }
+        'due_date': today },
+        
+        'recipient' : {
+        'name': 'Acme Corp',
+        'email': 'john@example.com',
+        'addr_1': '12345 Sunny Road',
+        'addr_2' : 'Sunnyville, CA 12345'
+        },
 
-    items = [{
+    'items' : [{
                 'charge': 300.0,
                 'title': 'website design'
             },
@@ -38,6 +36,11 @@ def invoice():
                 'title': 'Domain name (1 year)'
             }
         ]
+    }
+
+    details= posted_data.get('details', default_data['details'])
+    recipient = posted_data.get('recipient', default_data['recipient'])
+    items = posted_data.get('items', default_data['items'])
     
     total = sum([i['charge'] for i in items])
 
@@ -58,29 +61,6 @@ def invoice():
                 io.BytesIO(pdf),
                 download_name= f'{recipient["name"]} invoice.pdf'
             )
-
-
-# @app.route("/download")
-# def generate_pdf():
-
-#     # Set the path to wkhtmltopdf executable file
-#     path_wkthmltopdf = b'C:\Program Files\wkhtmltopdf\\bin\wkhtmltopdf.exe'
-#     config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
-
-#     # PDF Content
-#     name = 'Folafolu'
-#     email = 'ofolafolu@gmail.com'
-
-#     html = f"<html><body><h1>Hi {name}</h1><h2>Your email is {email}</h2></body></html>"
-#     pdf = pdfkit.from_string(html, False, configuration=config)
-
-#     headers = {
-#         'Content-Type': 'application/pdf',
-#         'Content-Disposition': f"attachment;filename={name}.pdf"
-#     }
-
-#     response = Response(pdf, headers=headers)
-#     return response
 
 
 if __name__ == '__main__':
